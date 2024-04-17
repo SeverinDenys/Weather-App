86ecb3723e101bc138f0865fc31818d2aa12c226
Denys Severin <denys.severin@gmx.ch>
2024-04-17 14:58:32 +0200

    adding adjustments for input section and 2 buttons

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,15 @@ searchBtn.addEventListener("click", (e) => {
         console.log("User Data:", userData);
         const location = locationInput;
         chosenCity.innerHTML = location;
-        const weatherTemperature = Math.floor(userData.list[0].main.temp); // -273.15 conversion from Kelvin to Celcius
-        temp.innerHTML = `${weatherTemperature} °C`; //display temperature in Kelvin
+        const weatherTemperature = Math.floor(userData.list[0].main.temp);
+        temp.innerHTML = `${weatherTemperature} °C`;
         const weatherMain = userData.list[0].weather[0].main;
         const weatherDescription = userData.list[0].weather[0].description;
-
         description.innerHTML = weatherMain;
 
         container.appendChild(imgElement);
+
+        // set the image based on weather conditions
 
         if (weatherMain === "Rain" && weatherDescription === "light rain") {
           imgElement.src = "./src/images/rain2.png";
@@ -81,17 +82,53 @@ searchBtn.addEventListener("click", (e) => {
           imgElement.src = "./src/images/thunderstorm.png";
         } else if (weatherMain === "Snow" && weatherDescription === "snow") {
           imgElement.src = "./src/images/snow.png";
+        } else if (
+          weatherMain === "Snow" &&
+          weatherDescription === "light snow"
+        ) {
+          imgElement.src = "./src/images/snow.png";
         } else if (weatherMain === "Mist" && weatherDescription === "mist") {
           imgElement.src = "./src/images/mist.png";
         }
+
+        // Reset input field after displaying weather information
+        resetInput();
+
+        // display future options button and h4 title
+        displayFutureOptions();
       })
       .catch((error) => {
         console.error("Error:", error);
       });
   };
 
+  const resetInput = () => {
+    document.getElementById("locationInput").value = "";
+  };
+
+  const displayFutureOptions = () => {
+    const weatherInfoFutureDescription = document.querySelector(
+      ".weather-info-future-description"
+    );
+    const searchBtn2 = document.getElementById("searchBtn2");
+    const searchBtn3 = document.getElementById("searchBtn3");
+
+    weatherInfoFutureDescription.style.visibility = "visible";
+    searchBtn2.style.visibility = "visible";
+    searchBtn3.style.visibility = "visible";
+  };
+
   console.log(getWeatherTemperature());
 });
 
 // Problems encountered so far ///
-// Some of the cities are wrong. Like when I type Rome it gives me the city in US but not in Italy
+// how to empty the input value text after click so the text doesn't stay in input field
+
+// display container background
+// const displayBackground = () => {
+//   const weatherInfoBackground = document.getElementById("weather-info");
+//   weatherInfoBackground.style.backgroundColor =
+//     "linear-gradient(135deg, rgba(255, 100, 100, 1) 0%, rgb(244, 244, 119) 100%)";
+// };
+
+// displayBackground();
